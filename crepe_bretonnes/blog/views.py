21dbe45c0120b377c3c86de7eca3feeeca20a6d1
abdelhak NASSEUR <a.nasseur@outlook.com>
2020-04-06 01:02:56 +0100

    first commit

---
+++ crepe_bretonnes/blog/views.py
@@ -0,0 +1,21 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+# Create your views here.
+
+def home(request):
+    """ Exemple de page non valide au niveau HTML pour que l'exemple soit concis """
+    return HttpResponse("""
+        <h1>Bienvenue sur mon blog !</h1>
+        <p>Les crêpes bretonnes ça tue des mouettes en plein vol !</p>
+    """)
+
+def view_article(request, id_article):
+    """ 
+    Vue qui affiche un article selon son identifiant (ou ID, ici un numéro)
+    Son ID est le second paramètre de la fonction (pour rappel, le premier
+    paramètre est TOUJOURS la requête de l'utilisateur)
+    """
+    return HttpResponse(
+        "Vous avez demandé l'article n° {0} !".format(id_article)    
+    )
